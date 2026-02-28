75c1cfc4f46344277afdf01b73a8dd9a91711c85
davelopez. <mrdavelopez28@gmail.com>
2026-02-28 18:36:57 +0800

    Create useProjects.ts

---
+++ src/hooks/useProjects.ts
@@ -0,0 +1,57 @@
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { getAll, getOne, catchError } from "./controller";
+import type { Project } from "@/data/types";
+import api from "./api/config";
+import { toast } from "sonner";
+
+
+const PROJECTS = "projects";
+
+export const useProjects = () => {
+    return useQuery({
+        queryKey: [PROJECTS],
+        queryFn: () => getAll<Project[]>(PROJECTS),
+        select: (data) =>
+            data.map((project) => ({
+                ...project
+            })),
+        staleTime: 60 * 10 * 1000,
+    });
+};
+
+
+export const useProject = (id: number) => {
+    return useQuery({
+        queryKey: [PROJECTS, id],
+        queryFn: () => getOne<Project>(PROJECTS, id),
+        staleTime: 60 * 10 * 1000
+    });
+};
+
+
+export const useAddProject = <TData = unknown>() => {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: async ({ data, file}: {data: TData; file?: File}) => {
+            const formData = new FormData();
+            formData.append("data", JSON.stringify(data));
+
+
+            if(file) {
+            formData.append("file", file);
+            }
+
+
+            const response = await api.post(`index.php?resource=projects`, formData);
+            return response.data;
+        },
+
+        onSuccess: (data) => {
+            queryClient.refetchQueries({ queryKey: [PROJECTS]});
+            toast.success("SUccessfully added new Project");
+        },
+        onError: catchError,
+    });
+};
+
