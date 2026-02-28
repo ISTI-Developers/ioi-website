9df0d8d409813ea2333620ff8befa7c57ac75f39
davelopez. <mrdavelopez28@gmail.com>
2026-03-01 01:47:28 +0800

    insert project bullet info

--- src/components/pages/projects/projectdetails/Bullet.tsx
+++ src/components/pages/projects/projectdetails/Bullet.tsx
@@ -3,109 +3,154 @@ import { Button } from "@/components/ui/button";
 import { Form } from "@/components/ui/form";
 import FormCardContent from "@/components/layout/FormCardContent";
 import FormFieldTextArea from "../../forms/fields/FormFieldTextArea";
+
 import { useProject } from "@/hooks/useProjects";
+import { useAddPoint } from "@/hooks/useProjectPoint";
 
 interface BulletProps {
-  projectId: number;
+    projectId: number;
+    onSuccess?: () => void;
 }
 
-export default function Bullet({ projectId }: BulletProps) {
-  const form = useForm({
-    defaultValues: {
-      problem: "",
-      solution: "",
-      service: "",
-      result: "",
-    },
-    mode: "all",
-  });
+export default function Bullet({ projectId, onSuccess }: BulletProps) {
+    const form = useForm({
+        defaultValues: {
+            problem: "",
+            solution: "",
+            service: "",
+            result: "",
+        },
+        mode: "all",
+    });
 
-  const { data: project, isLoading } = useProject(projectId);
+    const { mutate } = useAddPoint();
 
-  if (isLoading) return <p>Loading bullets...</p>;
+    const { data: project, isLoading } = useProject(projectId);
 
-  const points = project?.points || [];
+    if (isLoading) return <p>Loading bullets...</p>;
 
-  const problems = points.filter((p) => p.type === "problem");
-  const solutions = points.filter((p) => p.type === "solution");
-  const services = points.filter((p) => p.type === "service");
-  const results = points.filter((p) => p.type === "result");
+    const points = project?.points || [];
 
-  return (
-    <Form {...form}>
-      <div className="space-y-10 gap-5">
-        <div className="flex gap-2">
-          <FormCardContent title="Problems">
-            <ul className="list-disc list-inside">
-              {problems.map((p) => (
-                <li key={p.point_id}>{p.content}</li>
-              ))}
-            </ul>
-            <FormFieldTextArea
-              control={form.control}
-              name="problem"
-              label="description"
-              placeholder="Add a problem..."
-            />
-            <div className="flex justify-end">
-                <Button type="submit" size="sm" className="mt-2">Add</Button>
+    const problems = points.filter((p) => p.type === "problem");
+    const solutions = points.filter((p) => p.type === "solution");
+    const services = points.filter((p) => p.type === "service");
+    const results = points.filter((p) => p.type === "result");
+
+
+
+    const onSubmit = (type: "problem" | "solution" | "service" | "result") => {   
+        const value = form.getValues(type);
+
+        if (!value?.trim()) return;
+
+             console.log(value);
+        mutate(
+            {
+                project_id: projectId,
+                content: value,
+                type,
+            },
+            {
+                onSuccess: () => {
+                    form.setValue(type, "");
+                    onSuccess?.();
+                },
+                onError: (err) => {
+                    console.error("Failed to add point", err);
+                }
+            }
+        )
+
+
+    }
+
+
+
+
+
+    return (
+        <Form {...form}>
+            <div className="space-y-10 gap-5">
+                <div className="flex gap-2">
+                    <FormCardContent title="Problems">
+                        <ul className="list-disc list-inside">
+                            {problems.map((p) => (
+                                <li key={p.point_id}>{p.content}</li>
+                            ))}
+                        </ul>
+                        <FormFieldTextArea
+                            control={form.control}
+                            name="problem"
+                            label="description"
+                            placeholder="Add a problem..."
+                        />
+                        <div className="flex justify-end">
+                            <Button
+                                type="submit"
+                                size="sm"
+                                className="mt-2"
+                                onClick={() => onSubmit("problem")}
+
+                            >
+                                Add
+                            </Button>
+                        </div>
+                    </FormCardContent>
+
+                    <FormCardContent title="Solutions">
+                        <ul>
+                            {solutions.map((p) => (
+                                <li key={p.point_id}>{p.content}</li>
+                            ))}
+                        </ul>
+                        <FormFieldTextArea
+                            control={form.control}
+                            name="solution"
+                            label=""
+                            placeholder="Add a solution..."
+                        />
+                        <div className="flex justify-end">
+                            <Button type="submit" size="sm" className="mt-2">Add</Button>
+                        </div>
+                    </FormCardContent>
+                </div>
+
+                <div className="flex gap-2">
+                    <FormCardContent title="Service Rendered">
+                        <ul>
+                            {services.map((p) => (
+                                <li key={p.point_id}>{p.content}</li>
+                            ))}
+                        </ul>
+                        <FormFieldTextArea
+                            control={form.control}
+                            name="service"
+                            label=""
+                            placeholder="Add a service..."
+                        />
+                        <div className="flex justify-end">
+                            <Button type="submit" size="sm" className="mt-2">Add</Button>
+                        </div>
+                    </FormCardContent>
+
+                    <FormCardContent title="Key Results">
+                        <ul>
+                            {results.map((p) => (
+                                <li key={p.point_id}>{p.content}</li>
+                            ))}
+                        </ul>
+                        <FormFieldTextArea
+                            control={form.control}
+                            name="result"
+                            label=""
+                            placeholder="Add a result..."
+                        />
+                        <div className="flex justify-end">
+                            <Button type="submit" size="sm" className="mt-2">Add</Button>
+                        </div>
+                    </FormCardContent>
+                </div>
             </div>
-          </FormCardContent>
-
-          <FormCardContent title="Solutions">
-            <ul>
-              {solutions.map((p) => (
-                <li key={p.point_id}>{p.content}</li>
-              ))}
-            </ul>
-            <FormFieldTextArea
-              control={form.control}
-              name="solution"
-              label=""
-              placeholder="Add a solution..."
-            />
-              <div className="flex justify-end">
-                <Button type="submit" size="sm" className="mt-2">Add</Button>
-            </div>
-          </FormCardContent>
-        </div>
-
-        <div className="flex gap-2">
-          <FormCardContent title="Service Rendered">
-            <ul>
-              {services.map((p) => (
-                <li key={p.point_id}>{p.content}</li>
-              ))}
-            </ul>
-            <FormFieldTextArea
-              control={form.control}
-              name="service"
-              label=""
-              placeholder="Add a service..."
-            />
-              <div className="flex justify-end">
-                <Button type="submit" size="sm" className="mt-2">Add</Button>
-            </div>
-          </FormCardContent>
-
-          <FormCardContent title="Key Results">
-            <ul>
-              {results.map((p) => (
-                <li key={p.point_id}>{p.content}</li>
-              ))}
-            </ul>
-            <FormFieldTextArea
-              control={form.control}
-              name="result"
-              label=""
-              placeholder="Add a result..."
-            />
-              <div className="flex justify-end">
-                <Button type="submit" size="sm" className="mt-2">Add</Button>
-            </div>
-          </FormCardContent>
-        </div>
-      </div>
-    </Form>
-  );
+        </Form>
+    );
 }
---
+++ src/hooks/useProjectPoint.ts
@@ -0,0 +1,35 @@
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { getAll, getOne, catchError } from "./controller";
+import type { Point } from "@/data/types";
+import api from "./api/config";
+import { toast } from "sonner";
+
+
+
+const PROJECTS = "projects";
+
+
+export const useAddPoint = () => {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: async (data: Point) => {
+              const formData = new FormData();
+            formData.append("data", JSON.stringify(data));
+           
+            const response = await api.post(`index.php?resource=points`, formData);
+            return response.data;
+        },
+
+        onSuccess: (_, variables) => {
+            queryClient.refetchQueries({
+                queryKey: [PROJECTS, variables.project_id],
+            });
+
+            toast.success("Successfully added new Point");
+        },
+
+        onError: catchError,
+
+    });
+};
