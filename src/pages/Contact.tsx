ea42b280bb23d04e2cb6192d9c234b904cd02a4f
mikai019 <mariamikaela.parayno@letran.edu.ph>
2026-03-06 16:05:12 +0800

    small modification to contacts

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -6,7 +6,7 @@ function Contacts () {
 
   return (
     <div className="w-full text-white overflow-x-hidden px-6 md:px-12 lg:px-24 flex flex-col space-y-20">
-      <div className="max-w-4xl mx-auto">
+      <div className="max-w-6xl mx-auto">
 
         <div className="mb-6 sm:mb-8">
           <h1 className="text-center text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-bold mb-3 sm:mb-4 break-words">
@@ -22,11 +22,10 @@ function Contacts () {
           <FeaturedImage section="contact_middle" />
         <ContactForm />
         </div>
-      </div>
-
-<div className='space-y-20'>
+        <div className='space-y-20'>
         <Clients />
       <FeaturedImage section = "contact_bottom" />
+      </div>
       </div>
     </div>
   )
