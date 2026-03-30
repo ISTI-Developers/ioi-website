4eddb0b4dbceb60e01bafa422645c9ff835aee7a
davelopez. <mrdavelopez28@gmail.com>
2026-03-30 17:02:15 +0800

    Update Clients.tsx

--- src/components/layout/home/Clients.tsx
+++ src/components/layout/home/Clients.tsx
@@ -2,30 +2,32 @@ import { LogoCarousel } from "../../ui/logo-carousel";
 import { useClients } from "@/hooks/useClients";
 
 export function Clients() {
-  const { data: clients, isLoading } = useClients();
+    const { data: clients, isLoading } = useClients();
 
-  if (isLoading) return <p>Loading clients...</p>;
+    if (isLoading) return <p>Loading clients...</p>;
 
     return (
-        <div className="w-full flex flex-col items-center md:px-10 lg:px-95">
+        <div className="w-full">
+            <h1 className="text-white text-2xl lg:text-4xl font-bold w-120">
+                Empowering Visionaries: Stories of Collaboration and Transformation
+            </h1>
 
-            <div className="w-full max-w-2xl text-left">
 
-                <span className="text-orange-500 ttext-primary lg:text-xl uppercase tracking-widest block mb-4">
-                    // CLIENTS & PARTNERS
-                </span>
+            <div className="flex justify-between mt-4">
 
-                <div className="leading-[1.1] lg:leading-none flex flex-col">
-                    <h1 className="text-white text-2xl lg:text-4xl font-bold w-120">
-                        Empowering Visionaries: Stories of Collaboration and Transformation
-                    </h1>
+                <div>
+                    <span className="text-white lg:text-lg">
+                        All Projects
+                    </span>
+                    {/* <img src = "arm21.svg" className="h-20 w-20 inline-block ml-2" alt="Arrow pointing right" /> */}
                 </div>
 
-                <p className="text-gray-400 text-xs sm:text-sm md:text-base lg:text-xl mt-6 w-120">
+
+                <p className="text-gray-400 text-xs sm:text-sm md:text-base lg:text-xl  w-120">
                     Our clients are more than just collaborators—they are visionaries who inspire us to push the boundaries of design and innovation.
                 </p>
-            </div>
 
+            </div>
             <div className="mt-30 w-full">
                 <LogoCarousel clients={clients ?? []} />
             </div>
