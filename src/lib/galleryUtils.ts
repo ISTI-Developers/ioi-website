8a2915b5a7e0f933987ab9ee309f447c2eafdac5
davelopez. <mrdavelopez28@gmail.com>
2026-04-07 16:56:54 +0800

    modified codes for gallery structure

--- src/lib/galleryUtils.ts
+++ src/lib/galleryUtils.ts
@@ -5,9 +5,64 @@ const bentoLayouts: Record<number, Record<number, string>> = {
   2: {
     1: "col-span-2 row-span-2",
     2: "col-span-2 row-span-2",
-  }
+  },
+  3: {
+    1: "col-span-2 row-span-1",
+    2: "col-span-1 row-span-1",
+    3: "col-span-1 row-span-1",
+  },
+  4: {
+    1: "col-span-2 row-span-1",
+    2: "col-span-2 row-span-1",
+    3: "col-span-2 row-span-1",
+    4: "col-span-2 row-span-1",
+  },
+  5: {
+    1: "col-span-2 row-span-1",
+    2: "col-span-1 row-span-1",
+    3: "col-span-1 row-span-1",
+    4: "col-span-2 row-span-1",
+    5: "col-span-2 row-span-1",
+  },
+  6: {
+    1: "col-span-1 row-span-1",
+    2: "col-span-2 row-span-1",
+    3: "col-span-1 row-span-1",
+    4: "col-span-1 row-span-1",
+    5: "col-span-2 row-span-1",
+    6: "col-span-1 row-span-1",
+  },
+  7: {
+    1: "col-span-2 row-span-1",
+    2: "col-span-1 row-span-1",
+    3: "col-span-1 row-span-1",
+    4: "col-span-1 row-span-1",
+    5: "col-span-1 row-span-1",
+    6: "col-span-2 row-span-1",
+    7: "col-span-4 row-span-1",
+  },
+  8: {
+    1: "col-span-2 row-span-1",
+    2: "col-span-1 row-span-1",
+    3: "col-span-1 row-span-1",
+    4: "col-span-1 row-span-1",
+    5: "col-span-1 row-span-1",
+    6: "col-span-2 row-span-1",
+    7: "col-span-2 row-span-2",
+    8: "col-span-1 row-span-2",
+  },
+    9: {
+    1: "col-span-2 row-span-1",
+    2: "col-span-1 row-span-1",
+    3: "col-span-1 row-span-1",
+    4: "col-span-1 row-span-1",
+    5: "col-span-1 row-span-1",
+    6: "col-span-2 row-span-1",
+    7: "col-span-2 row-span-2",
+    8: "col-span-1 row-span-2",
+    9: "col-span-1 row-span-2",
+  },
 };
-
 
 export function getBentoClass(totalImages: number, position: number): string {
   return bentoLayouts[totalImages]?.[position] || "col-span-1 row-span-1";
