6aae7032c32baf726dae9483ab0f2344218ed14c
davelopez. <mrdavelopez28@gmail.com>
2026-04-07 08:21:36 +0800

    Update button-portfolio.tsx

--- src/components/ui/button-portfolio.tsx
+++ src/components/ui/button-portfolio.tsx
@@ -6,13 +6,13 @@ interface ButtonPortfolioProps {
 
 function ButtonPortfolio({ types, activeFilter, onFilterChange }: ButtonPortfolioProps) {
   return (
-    <div className="mt-20">
+    <div className="mt-20 mx-auto mb-20">
       <div className="flex flex-wrap gap-4">
         {types.map((type) => (
           <button
             key={type}
             onClick={() => onFilterChange(activeFilter === type ? "All" : type)}
-            className={`text-lg text-center font-semibold border border-orange-500 rounded-full px-4 py-2 cursor-pointer uppercase transition-colors
+            className={`text-2xl text-center font-semibold border border-orange-500 rounded-full px-5 py-3 cursor-pointer uppercase transition-colors
               ${activeFilter === type
                 ? "bg-orange-500 text-white"
                 : "text-orange-500 hover:text-white hover:bg-orange-500"
