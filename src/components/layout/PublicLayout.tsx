82381bcbc47e174380eddb49f4b2c50cc5373033
davelopez. <mrdavelopez28@gmail.com>
2026-04-13 09:50:59 +0800

    layouts for admin and public

---
+++ src/components/layout/PublicLayout.tsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "./NavBar";
+import Footer from "./Footer";
+
+
+
+function PublicLayout() {
+    return (
+        <>
+        <Navbar />
+        <Outlet />
+        <Footer />
+        
+        </>
+    )
+}
+
+export default PublicLayout;
