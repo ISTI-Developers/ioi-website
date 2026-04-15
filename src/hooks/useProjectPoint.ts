8493af5c4eb95f7d2bea45b853df9617fd8851cd
davelopez. <mrdavelopez28@gmail.com>
2026-04-15 16:24:39 +0800

    Update useProjectPoint.ts

--- src/hooks/useProjectPoint.ts
+++ src/hooks/useProjectPoint.ts
@@ -1,4 +1,4 @@
-import {  useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { catchError } from "./controller";
 import type { Point } from "@/data/types";
 import api from "./api/config";
@@ -7,14 +7,15 @@ import { toast } from "sonner";
 
 
 const PROJECTS = "projects";
+const POINTS = "points";
 
 
 export const useAddPoint = () => {
     const queryClient = useQueryClient();
 
     return useMutation({
-        mutationFn: async (data: Point) => {           
-            const res = await api.post(`index.php?resource=points`, data);
+        mutationFn: async (data: Point) => {
+            const res = await api.post(`index.php?resource=${POINTS}`, data);
             return res.data;
         },
 
@@ -30,3 +31,30 @@ export const useAddPoint = () => {
 
     });
 };
+
+
+export const useUpdatePoint = <TData extends {}>() => {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: async ({ id, data }: { id: number; data: TData }) => {
+            const res = await api.put(`index.php?resource=${POINTS}&id=${id}`, data,
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }
+            );
+
+            return res.data;
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: [POINTS] });
+            toast.success("Point updated successfully");
+        },
+        onError: (err: any) => {
+            toast.error("Failed to update point", err);
+        },
+    });
+};
+
