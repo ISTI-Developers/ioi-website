953093b8d463a242ef47ecfe035bc5580706e260
mikai019 <mariamikaela.parayno@letran.edu.ph>
2026-04-15 16:31:59 +0800

    added the contacts header

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -3,13 +3,11 @@ import {ContactForm} from '../components/pages/forms/contact'
 function Contacts () {
   return (
     <div className="w-full text-white px-2 lg:px-25 flex flex-col mb-30">
-      <h1 className="font-bold text-[20.1vw] w-full text-center">
-        Contact us
-      </h1>
+      <img src="/contacts.gif" alt="Contact Us Gif" className="w-250 mx-auto block pt-30 pb-20" />
       <section className="grid grid-cols-1 lg:grid-cols-2 gap-10 items-start">
         <div className="space-y-5 text-base md:text-lg lg:text-xl text-gray-300 text-justify">
-          <p>hello@example.com</p>
-          <p>+63 9975134678</p>
+          <p>inquiry@innovationone.com.ph</p>
+          <p>+63 917 112 0767 - Francis Ceruma</p>
           <p>
             HPL Building, 60 Sen. Gil Puyat Ave.,<br />
             Makati City 1234, Philippines
