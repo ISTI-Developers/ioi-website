d20caa2621ada7d3b0793f5f5defe2f6d876a0ac
davelopez. <mrdavelopez28@gmail.com>
2026-04-20 16:51:15 +0800

    Update useVideo.ts
    created update hook

--- src/hooks/useVideo.ts
+++ src/hooks/useVideo.ts
@@ -10,12 +10,12 @@ const VIDEO = "video";
 export const useVideo = (id: number) => {
     return useQuery({
         queryKey: [VIDEO, id],
-        queryFn: () => getOne<{ video: Video[]}>(VIDEO, id),
+        queryFn: () => getOne<{ video: Video[] }>(VIDEO, id),
         staleTime: 60 * 10 * 1000
     });
 }
 
-export const useAddVideo = <TData=unknown>() => {
+export const useAddVideo = <TData = unknown>() => {
     const queryClient = useQueryClient();
 
     return useMutation({
@@ -24,9 +24,37 @@ export const useAddVideo = <TData=unknown>() => {
             return res.data;
         },
         onSuccess: (_data) => {
-            queryClient.refetchQueries({ queryKey: [VIDEO]});
+            queryClient.refetchQueries({ queryKey: [VIDEO] });
             toast.success("Successfully added new video");
         },
         onError: catchError,
     });
 }
+
+
+export const useUpdateVideo = <TData extends {}>() => {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: async ({ id, data }: { id: number, data: TData }) => {
+            const res = await api.put(`index.php?resource=${VIDEO}&id=${id}`, data,
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }
+            );
+
+            return res.data;
+        },
+
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: [VIDEO] });
+            toast.success("Video updated successfully");
+        },
+        onError: (err: any) => {
+            console.error("Failed to update video", err);
+            toast.error("Failed to update video", err);
+        },
+    });
+};
