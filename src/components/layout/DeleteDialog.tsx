fd57206728313ec323618ba131a65e7c7d260596
davelopez. <mrdavelopez28@gmail.com>
2026-04-20 16:51:43 +0800

    Create DeleteDialog.tsx

---
+++ src/components/layout/DeleteDialog.tsx
@@ -0,0 +1,43 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "../ui/alert-dialog";
+import { Button } from "../ui/button";
+
+interface DeleteDialogProps {
+  handleConfirm: () => void;
+  open: boolean;                         
+  onOpenChange: (open: boolean) => void; 
+}
+
+function DeleteDialog({ handleConfirm, open, onOpenChange }: DeleteDialogProps) {
+  return (
+    <AlertDialog open={open} onOpenChange={onOpenChange}>      
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete the and
+            remove from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction asChild>
+            <Button className="bg-red-700" onClick={handleConfirm}>
+              Delete
+            </Button>
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
+
+export default DeleteDialog;
