bf5738f024f850838c599adecbb3e7e600ee8758
davelopez. <mrdavelopez28@gmail.com>
2026-04-22 11:01:55 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import { AuthProvider } from './context/AuthContext.tsx';
 import { Toaster } from "sonner";
 import { setSession } from "./hooks/api/config.ts";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import ScrollToTop from './components/ScrollToTop.tsx';
 
 const token = localStorage.getItem("token");
 
@@ -20,6 +21,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
+        <ScrollToTop />
         <AuthProvider>
           <App />
         </AuthProvider>
