1ace6f0cee5d4ef2f671a13a34c70dc77ab5cabb
davelopez. <mrdavelopez28@gmail.com>
2026-04-22 08:56:01 +0800

    Update Sidebar.tsx

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,13 +1,18 @@
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "../ui/app-sidebar";
 import { Outlet } from "react-router-dom";
+import Menu from "./Menu";
 
 function Sidebar() {
   return (
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <main className="flex-1 p-6 ">
+        <div className="sticky top-0 z-10 ">
+          <Menu />
+        </div>
+
+        <main className="flex-1 overflow-auto p-6">
           <Outlet />
         </main>
       </SidebarInset>
