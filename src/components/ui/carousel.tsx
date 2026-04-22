65108f62d8d13c98f2616d366ceae5a39265e5d2
davelopez. <mrdavelopez28@gmail.com>
2026-04-22 08:56:15 +0800

    Update carousel.tsx

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -193,7 +193,7 @@ function CarouselPrevious({
       onClick={scrollPrev}
       {...props}
     >
-      <ArrowLeft />
+      <ArrowLeft className="w-4 h-4 text-black opacity-100" />
       <span className="sr-only">Previous slide</span>
     </Button>
   )
@@ -223,7 +223,7 @@ function CarouselNext({
       onClick={scrollNext}
       {...props}
     >
-      <ArrowRight />
+      <ArrowRight  className="w-4 h-4 text-black opacity-100"/>
       <span className="sr-only">Next slide</span>
     </Button>
   )
